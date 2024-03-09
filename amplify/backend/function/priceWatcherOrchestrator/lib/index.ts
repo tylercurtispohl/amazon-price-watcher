5d6f7195c43e0964b792703a8b63cb46944c3e96
Tyler Pohl <tylercurtispohl@gmail.com>
2024-03-09 13:22:22 -0800

    add priceWatcherOrchestrator and configure typescript for lambda

---
+++ amplify/backend/function/priceWatcherOrchestrator/lib/index.ts
@@ -0,0 +1,16 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+  return {
+    statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  },
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+};
