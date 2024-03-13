cdf221b672663878ea1129825acc0b38c293dbda
Tyler Pohl <tylercurtispohl@gmail.com>
2024-03-13 08:21:57 -0700

    Remove the PriceMessengerQueue

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,39 +1,36 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "amzpricewatcher": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  },
-  "function": {
-    "amzpricewatcherpriceWatcherLayer": {
-      "Arn": "string"
-    },
-    "orchestrator": {
-      "Arn": "string",
-      "CloudWatchEventRule": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "priceWatcher": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  },
-  "queue": {
-    "priceWatcherQueue": {
-      "PriceMessengerQueueARN": "string",
-      "PriceMessengerQueueName": "string",
-      "PriceMessengerQueueURL": "string",
-      "QueueARN": "string",
-      "QueueName": "string",
-      "QueueURL": "string"
-    }
-  }
-}
+  api: {
+    amzpricewatcher: {
+      GraphQLAPIEndpointOutput: "string";
+      GraphQLAPIIdOutput: "string";
+      GraphQLAPIKeyOutput: "string";
+    };
+  };
+  function: {
+    amzpricewatcherpriceWatcherLayer: {
+      Arn: "string";
+    };
+    orchestrator: {
+      Arn: "string";
+      CloudWatchEventRule: "string";
+      LambdaExecutionRole: "string";
+      LambdaExecutionRoleArn: "string";
+      Name: "string";
+      Region: "string";
+    };
+    priceWatcher: {
+      Arn: "string";
+      LambdaExecutionRole: "string";
+      LambdaExecutionRoleArn: "string";
+      Name: "string";
+      Region: "string";
+    };
+  };
+  queue: {
+    priceWatcherQueue: {
+      QueueARN: "string";
+      QueueName: "string";
+      QueueURL: "string";
+    };
+  };
+};
